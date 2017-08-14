368ca1cc7ea4cf7f32f639c3ddd4622bcfde069b
weiwosuoai <weiwosuoai1991@gmail.com>
2017-08-14 16:48:33 +0800

    answer pojo

---
+++ src/main/java/site/exception/pojo/Answer.java
@@ -0,0 +1,137 @@
+package site.exception.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Answer implements Serializable {
+    private static final long serialVersionUID = -7446783718968300806L;
+    private Integer id;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    private Integer createUserId;
+
+    private Integer questionId;
+
+    private Integer voteUp;
+
+    private Integer voteDown;
+
+    private Integer language;
+
+    private Integer originalId;
+
+    private String answer;
+
+    private String answerCh;
+
+    private Integer isAccepted;
+
+    private Integer voteCount;
+
+    public String getAnswerCh() {
+        return answerCh;
+    }
+
+    public void setAnswerCh(String answerCh) {
+        this.answerCh = answerCh;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Integer getIsAccepted() {
+        return isAccepted;
+    }
+
+    public void setIsAccepted(Integer isAccepted) {
+        this.isAccepted = isAccepted;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(Integer createUserId) {
+        this.createUserId = createUserId;
+    }
+
+    public Integer getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(Integer questionId) {
+        this.questionId = questionId;
+    }
+
+    public Integer getVoteUp() {
+        return voteUp;
+    }
+
+    public void setVoteUp(Integer voteUp) {
+        this.voteUp = voteUp;
+    }
+
+    public Integer getVoteDown() {
+        return voteDown;
+    }
+
+    public void setVoteDown(Integer voteDown) {
+        this.voteDown = voteDown;
+    }
+
+    public Integer getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Integer language) {
+        this.language = language;
+    }
+
+    public Integer getOriginalId() {
+        return originalId;
+    }
+
+    public void setOriginalId(Integer originalId) {
+        this.originalId = originalId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer == null ? null : answer.trim();
+    }
+}
